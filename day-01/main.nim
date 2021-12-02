7aacfb82cb94da0c84783b1947afd582e5c7bfd7
Piyush Rungta <piyushrungta25@gmail.com>
2021-12-02 17:42:21 +0530

    Pass seq as reference

--- day-01/main.nim
+++ day-01/main.nim
@@ -9,18 +9,18 @@ proc parseInput(): seq =
     return f.lines.toSeq().map(x => parseInt(x))
 
 
-proc part1(depths: seq): int =
+proc part1(depths: var seq[int]): int =
     for i in 1..<depths.len:
         if depths[i-1] < depths[i]:
             result += 1
 
-proc part2(depths: seq): int = 
+proc part2(depths: var seq[int]): int = 
     let window = 3
     for i in window..<depths.len:
         if depths[i-window] < depths[i]:
             result += 1
 
 when isMainModule:
-  let depths = parseInput()
+  var depths = parseInput()
   echo part1(depths)
   echo part2(depths)
